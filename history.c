304616b2712c497bd6c61e5c0d8a685e45d2ef46
gstarkey1 <gstarkey1@agora.cs.wcu.edu>
2019-03-22 17:02:27 -0400

    Worked on history.c

--- history.c
+++ history.c
@@ -2,19 +2,40 @@
 #define HIST_SIZE 40
 // TODO: Stuff in here to save and list history
 
-int main(void) {
 
-    char commands[HIST_SIZE];
-}
+char commands[HIST_SIZE];
+static int count = 0;
 
-char get_Commands(char** executedCommand){
+char add_history(char** executedCommand){
+    
+    if(count == (HIST_SIZE - 1)) {
+        count = 0;
+    }
 
-    static int count = 0;
     commands[count] = executedCommand;
+    count++;
 }
 
-void do_history(char** args)
+void print_commmands(void) {
 
+    int i;
+
+    //loops through commands array and prints all values
+    
+    for(i = 0; i < commands.length; i++) {
+
+        printf("%d: %s", i, commands[i]);
+
+    }
+
+}
+
+void print_single_command(int history_num) {
+
+    //Prints a single command that corresponds to command argument
+    printf("%d: %s", history_num, commands[history_num]);
+
+}
 
     
 
